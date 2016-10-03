c31d688ceca25ba50092b3b0a5123efd20d7ca23
rcordero <rcor.cr@gmail.com>
2016-10-03 00:57:27 -0300

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: wordpress
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
+
+#include_recipe 'wordpress::apache'
+include_recipe 'wordpress::php5'
+include_recipe 'wordpress::mysql'
+include_recipe 'wordpress::wordpress'
