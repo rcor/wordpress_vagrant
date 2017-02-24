a8c4d8eac0cbd124c664c0fecba8b578cb3b59b9
rcordero <rcor.cr@gmail.com>
2017-02-23 23:06:14 -0300

    update from php5 to php7

--- recipes/php7.rb
+++ recipes/php7.rb
@@ -1,5 +1,5 @@
 
-%w(php5 libapache2-mod-php5 php5-mcrypt php5-mysql).each do |package|
+%w(php7 libapache2-mod-php7 php7-mcrypt php7-mysql).each do |package|
   package 'Install #{package}' do
     package_name package
     action :install
