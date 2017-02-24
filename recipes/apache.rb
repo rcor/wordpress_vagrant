db79bee07fd3e065a8e3df6019e136f9328b7843
rcordero <rcor.cr@gmail.com>
2017-02-24 00:03:47 -0300

    add clean recipe

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -2,10 +2,7 @@ package 'Install Apache' do
   package_name 'apache2'
 end
 
-file '/var/www/html/index.html' do
-  action :delete
-  ignore_failure true
-end
+
 
 service 'apache2' do
   action :nothing
---
+++ recipes/clean.rb
@@ -0,0 +1,4 @@
+file '/var/www/html/index.html' do
+  action :delete
+  ignore_failure true
+end
